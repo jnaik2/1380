72abd2c6f49aa9e3b36365285e4cc28a6d79592b
Jaideep Naik <jaideep_naik@brown.edu>
2025-02-11 00:10:25 -0500

    student tests

--- test/test-student/m2.student.test.js
+++ test/test-student/m2.student.test.js
@@ -6,31 +6,120 @@
     Imporant: Do not modify any of the test headers (i.e., the test('header', ...) part). Doing so will result in grading penalties.
 */
 
-const distribution = require('../../config.js');
-
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+const distribution = require("../../config.js");
+test("(1 pts) student test", (done) => {
+  // Test status with a missing configuration to ensure error handling
+  distribution.local.status.get(null, (e, v) => {
+    try {
+      expect(e).toBeTruthy();
+      expect(e).toBeInstanceOf(Error);
+      expect(v).toBeFalsy();
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
 });
 
+test("(1 pts) student test", (done) => {
+  // Test routes put() then rem() then get() to ensure the service is removed
+  const routes = distribution.local.routes;
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const testService = {};
+
+  testService.method = () => {
+    return "method";
+  };
+  routes.put(testService, "method", (_, v) => {
+    routes.rem(v, (_, v) => {
+      routes.get(v, (e, v) => {
+        try {
+          expect(e).toBeTruthy();
+          expect(e).toBeInstanceOf(Error);
+          expect(e.message).toBe("Value not accessible in service");
+          expect(v).toBeFalsy();
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
 });
 
+test("(1 pts) student test", (done) => {
+  // Test routes consecutive puts to ensure the service is updated with latest
+  const routes = distribution.local.routes;
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  const testService1 = {};
+  const testService2 = {};
+
+  testService1.method = () => {
+    return "method1";
+  };
+  testService2.method = () => {
+    return "method2";
+  };
+  routes.put(testService1, "method", (_, v) => {
+    routes.put(testService2, "method", (_, v) => {
+      routes.get(v, (e, v) => {
+        try {
+          expect(e).toBeFalsy();
+          expect(v).toBe(testService2);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
 });
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+test("(1 pts) student test", (done) => {
+  // Test send() with malformed inputs to ensure error handling
+  const node = distribution.node.config;
+  const remote = { node: node, service: "status" };
+  const message = ["invalid"];
+
+  distribution.local.comm.send(message, remote, (e, v) => {
+    try {
+      expect(e).toBeTruthy();
+      expect(e).toBeInstanceOf(Error);
+      expect(v).toBeFalsy();
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
 });
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+test("(1 pts) student test", (done) => {
+  // Test send() with missing node to ensure error handling
+  const remote = { service: "status", method: "get" };
+  const message = ["sid"];
+
+  distribution.local.comm.send(message, remote, (e, v) => {
+    try {
+      expect(e).toBeTruthy();
+      expect(e).toBeInstanceOf(Error);
+      expect(v).toBeFalsy();
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
+
+let localServer = null;
+
+beforeAll((done) => {
+  distribution.node.start((server) => {
+    localServer = server;
+    done();
+  });
+});
+
+afterAll((done) => {
+  localServer.close();
+  done();
 });
