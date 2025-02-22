1182e9045c7cf271d402fb6a16a7534c36cbc201
Jaideep Naik <jaideep_naik@brown.edu>
2025-02-22 06:10:46 +0000

    store & mem local working

--- distribution/local/mem.js
+++ distribution/local/mem.js
@@ -1,11 +1,36 @@
+const {id} = require("../util/util");
+let data = new Map();
 
 function put(state, configuration, callback) {
+    let key = configuration;
+
+    if (!configuration) {
+        key = id.getID(state);
+    }
+
+    data.set(key, state);
+    callback(null, state);
 };
 
 function get(configuration, callback) {
+    let value = data.get(configuration);
+
+    if (value) {
+        callback(null, value);
+    } else {
+        callback(new Error("No value found for key: " + configuration), null);
+    }
 }
 
 function del(configuration, callback) {
+    let value = data.get(configuration);
+
+    if (value) {
+        data.delete(configuration);
+        callback(null, value);
+    } else {
+        callback(new Error("No value found for key: " + configuration), null);
+    }
 };
 
 module.exports = {put, get, del};
--- distribution/local/store.js
+++ distribution/local/store.js
@@ -3,15 +3,70 @@
 - Use absolute paths to make sure they are agnostic to where your code is running from!
   Use the `path` module for that.
 */
+const { id, serialize, deserialize } = require("../util/util");
+const fs = require('fs');
+const path = require('path');
 
 
 function put(state, configuration, callback) {
+  let nid = global.moreStatus["nid"];
+  let key = configuration;
+  if (!configuration) {
+    key = id.getID(state);
+  }
+
+  let filepath = path.join(process.cwd(), `/store/${nid}/${key}`);
+  let dirPath = path.dirname(filepath);
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, {recursive: true});
+  }
+
+  fs.writeFile(filepath, serialize(state), (err) => {
+    if (err) {
+      callback(new Error(err), null);
+    } else {
+      callback(null, state);
+    }
+  })
 }
 
 function get(configuration, callback) {
+  let nid = global.moreStatus["nid"];
+  let filepath = path.join(process.cwd(), `/store/${nid}/${configuration}`);
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      callback(new Error(err), null);
+    } else {
+      let obj = deserialize(data.toString());
+      callback(null, obj);
+    }
+  })
 }
 
 function del(configuration, callback) {
+  let nid = global.moreStatus["nid"];
+  let filepath = path.join(process.cwd(), `/store/${nid}/${configuration}`);
+
+  if (!fs.existsSync(filepath)) {
+    callback(new Error("No value found for key: " + configuration), null);
+    return
+  } else {
+    get(configuration, (err, data) => {
+      if (err) {
+        callback(new Error(err), null);
+      } else {
+        fs.unlink(filepath, (err) => {
+          if (err) {
+            callback(new Error(err), null);
+          } else {
+            callback(null, data);
+          }
+        })
+      }
+    })
+
+  }
 }
 
 module.exports = {put, get, del};
